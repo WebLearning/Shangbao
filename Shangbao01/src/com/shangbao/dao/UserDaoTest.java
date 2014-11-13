cda94ad06e1885b32041c04c0c4cbde4e23b3fb5
yiyangstd <yangyistd@163.com>
2014-11-13 18:59:50 +0800

    使用sha加密密码

--- Shangbao01/src/com/shangbao/dao/UserDaoTest.java
+++ Shangbao01/src/com/shangbao/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.shangbao.dao;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
 
 import com.shangbao.model.User;
 
@@ -10,9 +11,10 @@ public class UserDaoTest {
 	public static void main(String[] args) {
 		BeanFactory applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserDao dao = (UserDao)applicationContext.getBean("userDaoImp");
+		PasswordEncoder encoder = (PasswordEncoder)applicationContext.getBean("passwordEncoder");
 		User u = new User();
-		u.setName("t1");
-		u.setPasswd("123");
+		u.setName("yy");
+		u.setPasswd(encoder.encodePassword("123", null));
 		u.setRole("ROLE_USER");
 		dao.insert(u);
 	}
