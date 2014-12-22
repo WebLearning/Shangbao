4576d0cc7127cfc26be05a3816c4c1805696f262
qk0106 <qk0106@sina.com>
2014-12-22 10:27:14 +0800

    debug

--- Shangbao01/WebRoot/WEB-SRC/src/js/dashboard/controllers/article-ctrl.js
+++ Shangbao01/WebRoot/WEB-SRC/src/js/dashboard/controllers/article-ctrl.js
@@ -8,54 +8,78 @@ angular.module("Dashboard").controller("articleCtrl", ["$scope","$http", functio
      $scope.editorContent = data;
      }); */
 
-    var tempData=
-        {"mainHeading":"成绵乐客专“蓄势待发” 应急救援演练10天",
-        "subHeading":"即将进入运行试验阶段 全真模拟开行动车组的情形",
-        "source":"成都商报电子版",
-        "keyWords":"成绵乐、动车组、救援演练",
-        "sorts":{"原创":true,"本地":true,"热门":false,"四川":true,"娱乐":false,"时事":true,"体育":false,"财经":false},
-        "textContent":'<p style="text-indent: 2em;">据了解，为期10天的应急救援演练基本上覆盖了成绵乐客专上的各个车站与区段，应急救援演练的项目至少达到50项以上。200公里时速 模拟运营真实状况 在应急救援演练项目之外，还将专门模拟列车开通运营，模仿运行时刻表，进行行车试验。</p><p style="text-indent: 2em;"><img src="img/spiderMan.jpg"/></p><p style="text-indent: 2em;">据介绍，通过运行图参数测试，掌握全程运行时分，行车试验则对各项设备进行考验，检验各系统和整体系统的稳定性及能力，并且让客运、车站、通信等各个专业的人员得到实作培训，熟悉各种规章制度，熟悉工作流程。</p><p style="text-indent: 2em;"><img src="img/shitMan.jpg"/></p><p style="text-indent: 2em;">据了解，这部分试验将采用CRH1A或CRH2A型动车组，动车组将模拟北向、南向行驶，或全程拉通行驶的运行方式，每种模拟方式基本都是站站停，行进中保持时速200公里，从江油至峨眉山大约需要2个多小时，成都至江油、成都至峨眉山大约各在1个小时左右。</p>'
-        };
+    $scope.recvData={
+        title:"成绵乐客专“蓄势待发” 应急救援演练10天",
+        subTitle:"即将进入运行试验阶段 全真模拟开行动车组的情形",
+        keyWord:["成绵乐","动车组","救援演练"],
+        author:"杨一",
+        summary:"成绵乐客专“蓄势待发”",
+        content:'<p style="text-indent: 2em;">据了解，为期10天的应急救援演练基本上覆盖了成绵乐客专上的各个车站与区段，应急救援演练的项目至少达到50项以上。200公里时速 模拟运营真实状况 在应急救援演练项目之外，还将专门模拟列车开通运营，模仿运行时刻表，进行行车试验。</p><p style="text-indent: 2em;"><img src="img/spiderMan.jpg"/></p><p style="text-indent: 2em;">据介绍，通过运行图参数测试，掌握全程运行时分，行车试验则对各项设备进行考验，检验各系统和整体系统的稳定性及能力，并且让客运、车站、通信等各个专业的人员得到实作培训，熟悉各种规章制度，熟悉工作流程。</p><p style="text-indent: 2em;"><img src="img/shitMan.jpg"/></p><p style="text-indent: 2em;">据了解，这部分试验将采用CRH1A或CRH2A型动车组，动车组将模拟北向、南向行驶，或全程拉通行驶的运行方式，每种模拟方式基本都是站站停，行进中保持时速200公里，从江油至峨眉山大约需要2个多小时，成都至江油、成都至峨眉山大约各在1个小时左右。</p>',
+        time:new Date(),
+        channel:["原创","本地","热门"],
+        picturesUrl:["http://localhost:8080/Shangbao01/WEB-SRC/src/img/shitMan.jpg"],
+        level:"一级",
+        from:"成都商报"
+    };
 
-    $scope.recvData=
-        {"mainHeading":"",
-        "subHeading":"",
-        "source":"",
-        "keyWords":"",
-        "sorts":{"原创":false,"本地":false,"热门":false,"四川":false,"娱乐":false,"时事":false,"体育":false,"财经":false},
-        "pictures":[],
-        "textContent":""
+    $scope.articleData={
+        title:"",
+        subTitle:"",
+        keyWord:new Array(),
+        author:"",
+        summary:"",
+        content:'',
+        time:null,
+        channel:[],
+        picturesUrl:[],
+        level:"",
+        from:""
         };
 
     $scope.getEditorContent=function()
     {
         //导入数据
-        for(p in tempData){
-            if(p=="sorts"){
+        for(p in $scope.recvData){
+            if(p=="keyWord"||p=="channel"||p=="picturesUrl"){
                 for(i in $scope.recvData[p]){
-                    $scope.recvData[p][i]=tempData[p][i];
+                    $scope.articleData[p][i]=$scope.recvData[p][i];
                 }
             }else{
-                $scope.recvData[p]=tempData[p];
+                $scope.articleData[p]=$scope.recvData[p];
             }
         }
     };
 
     $scope.testLog=function()
     {
-        console.log(tempData);
         console.log($scope.recvData);
+        console.log($scope.articleData);
     };
+
 
     $scope.changeSort=function(sortName)
     {
         $scope.recvData.sorts[sortName]=!$scope.recvData.sorts[sortName];
     };
 
+    $scope.saveArticle=function(){
+        var jsonString=JSON.stringify($scope.articleData);
+        $http.post('http://localhost:8080/Shangbao01/article/newArticle',jsonString).success(function(data) {
+            console.log("success save testData");
+        });
+    };
+
+    $scope.putArticle=function(){
+        var jsonString=JSON.stringify($scope.articleData);
+        $http.put('http://localhost:8080/Shangbao01/article/newArticle',jsonString).success(function(data) {
+            console.log("success put testData");
+        });
+    };
 
 
 
 
+    //关于上传图片的----------------------------------------------------------------------------------------------
 
     //当input file选择的文件有变化时
     $scope.onInputChange=function(inputFileObj)
@@ -180,6 +204,8 @@ angular.module("Dashboard").controller("articleCtrl", ["$scope","$http", functio
     {
         $('#myModal_addIMG').modal('toggle');
     };
+
+    //关于上传图片的----------------------------------------------------------------------------------------------
 
 
 }]);
