03a8ea8683d39d4b749f215c7f188f32520b6c81
yiyangstd <yangyistd@163.com>
2015-01-05 20:29:18 +0800

    修改添加评论

--- Shangbao01/test/com/shangbao/app/service/UserIdentifyServiceTest.java
+++ Shangbao01/test/com/shangbao/app/service/UserIdentifyServiceTest.java
@@ -3,6 +3,8 @@ package com.shangbao.app.service;
 import static org.junit.Assert.*;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
@@ -17,13 +19,17 @@ public class UserIdentifyServiceTest {
 	public void test() {
 		BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserIdentifyService identiy = (UserIdentifyService) factory.getBean("userIdentifyService");
-		RemoteUser user = identiy.new RemoteUser();
-		user.setBirthdaty(System.currentTimeMillis()/1000);
-		user.setEmail("yangyistd@163.com");
-		user.setNickname("一梦醉千年");
-		user.setPsw("330810852");
-		user.setSex(1);
-		//identiy.addUser(user);
+//		RemoteUser user = identiy.new RemoteUser();
+//		user.setBirthdaty(System.currentTimeMillis()/1000);
+//		user.setEmail("yangyistd@163.com");
+//		user.setNickname("一梦醉千年");
+//		user.setPsw("330810852");
+//		user.setSex(1);
+		Map user = new HashMap<String, String>();
+		user.put("phone", "123123");
+		user.put("email", "sdfsdf@123.com");
+		user.put("qq", "123123");
+		identiy.addUser(user);
 	}
 
 }
