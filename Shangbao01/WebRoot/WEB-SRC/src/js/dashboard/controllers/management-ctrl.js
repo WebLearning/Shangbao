f6f9bb073a7e0ee04ee8b513a91095e93af2b79b
zhangyini <945346213@qq.com>
2015-01-12 20:36:23 +0800

    manage

---
+++ Shangbao01/WebRoot/WEB-SRC/src/js/dashboard/controllers/management-ctrl.js
@@ -0,0 +1,37 @@
+/**
+ managementCtrl.js
+ **/
+angular.module("Dashboard").controller("managementCtrl",["$scope","$http",function($scope,$http){
+
+    $scope.refreshLog=function(){
+        $scope.getAppNames();
+    };
+
+    $scope.appNames=[];
+    $scope.getAppNames=function(){
+        var url=$scope.projectName+'/channel/channels';
+        $http.get(url).success(function(data){
+            console.log(data);
+            if(data.length>0){
+                for(i=0;i<data.length;i++){
+                        $scope.appNames.push(data[i]);
+                        $scope.getModelNames(data[i].englishName)
+                }
+            }
+        });
+    };
+    $scope.getAppNames();
+
+    $scope.modelNames=[];
+    $scope.getModelNames=function(channelName){
+        var url=$scope.projectName+'/channel/'+channelName+'/channels';
+        $http.get(url).success(function(data){
+            console.log(data);
+            if(data.length>0){
+                for(i=0;i<data.length;i++){
+                    $scope.modelNames.push(data[i]);
+                }
+            }
+        });
+    };
+}]);
