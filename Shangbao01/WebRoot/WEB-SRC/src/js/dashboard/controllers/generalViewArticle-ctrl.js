820abc33a661100e010e58d13b6ab304e5030a37
zhangyini <945346213@qq.com>
2015-03-15 19:02:03 +0800

    ff

---
+++ Shangbao01/WebRoot/WEB-SRC/src/js/dashboard/controllers/generalViewArticle-ctrl.js
@@ -0,0 +1,25 @@
+angular.module("Dashboard").controller("generalViewArticleCtrl", ["$scope","$http", function ($scope,$http) {
+
+    $scope.goGeneral=function()
+    {
+        $scope.clearArticle();
+        document.getElementById("generalViewArticle").className="tab-pane";
+        document.getElementById("generalView").className="tab-pane active";
+        document.getElementById("generalSidebarID").className="sidebar-list";
+        $scope.getNewGeneralViewData();
+    };
+    $scope.clearArticle=function()
+    {
+        for(p in $scope.articleData){
+            if(p=="keyWord"||p=="channel"||p=="picturesUrl"||p=="logs"){
+                $scope.articleData[p]=[];
+            }else if(p=="author"||p=="content"||p=="from"||p=="subTitle"||p=="time"||p=="title"||p=="summary"||p=="activity"||p=="outSideUrl"){
+                $scope.articleData[p]="";
+            }else{
+                $scope.articleData[p]=null;
+            }
+        }
+//        $scope.calculateWords();
+//        $scope.calculatePictures();
+    };
+}]);
